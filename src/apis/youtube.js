95edd8cba5909ed2f544b098de756444ec542382
FK <f.kusztelak@gmail.com>
2019-05-16 18:17:03 +0200

    SearchBar + Api Call

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyBLugoiskba2Cf1QX4g1aFflbiwnvZLYu4";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
